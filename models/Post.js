a7d7d871b01f0a37fda91e092bb79a468de47b34
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-10 12:27:21 -0500

    Add description field to model.

--- models/Post.js
+++ models/Post.js
@@ -7,6 +7,9 @@ const PostSchema = new mongoose.Schema ({
     image: {
         type: String
     },
+    description: {
+        type: String
+    },
     createdAt: {
         type: Date,
         default: Date.now
