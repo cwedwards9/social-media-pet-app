92a5cbeddaa1ea7f9a1345543520f0bc760c9b89
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-10 12:31:31 -0500

    Add to index route to find all data in the Post collection in the db. Add a GET route for a form to create a new post. Add a GET route for showing a specific post.

--- routes/pets.js
+++ routes/pets.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
+// INDEX route - show all pet posts
 router.get("/pets", (req, res) => {
-    res.render("pets/index");
+    db.Post.find({})
+        .then(pets => {
+            res.render("pets/index", {pets: pets});
+        });
 });
 
+
+// NEW route - create a new post of your pet
+router.get("/pets/new", (req, res) => {
+    res.render("pets/new");
+});
+
+
+// SHOW route - display more info about the pet post
+router.get("/pets/:id", (req, res) => {
+    db.Post.findById(req.params.id)
+        .populate("users")
+        .then(foundPet => {
+            res.render("pets/show", {pet: foundPet});
+        });
+});
 
 module.exports = router;
