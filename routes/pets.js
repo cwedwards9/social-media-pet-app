8e5347c2bfa82e805024dac410f7f9bdf6a2de91
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-11 12:26:34 -0500

    Add the create, edit, update, delete routes for posts.

--- routes/pets.js
+++ routes/pets.js
@@ -6,7 +6,7 @@ const db = require("../models");
 router.get("/pets", (req, res) => {
     db.Post.find({})
         .then(pets => {
-            res.render("pets/index", {pets: pets});
+            res.render("pets/index", {posts: pets});
         });
 });
 
@@ -16,14 +16,47 @@ router.get("/pets/new", (req, res) => {
     res.render("pets/new");
 });
 
+// CREATE route - add a new post to the database
+router.post("/pets", (req, res) => {
+    db.Post.create(req.body)
+        .then(() => {
+            res.redirect("/pets");
+        })
+});
+
 
 // SHOW route - display more info about the pet post
 router.get("/pets/:id", (req, res) => {
     db.Post.findById(req.params.id)
-        .populate("users")
-        .then(foundPet => {
-            res.render("pets/show", {pet: foundPet});
+        .then(foundPetPost => {
+            res.render("pets/show", {post: foundPetPost});
         });
+});
+
+
+// EDIT route - display edit form for a post
+router.get("/pets/:id/edit", (req, res) => {
+    db.Post.findById(req.params.id)
+        .then(foundPetPost => {
+            res.render("pets/edit", {post: foundPetPost});
+        })
+});
+
+// UPDATE route - update a post
+router.put("/pets/:id", (req, res) => {
+    db.Post.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/pets/${req.params.id}`);
+        })
+});
+
+
+// DELETE route - delete a post
+router.delete("/pets/:id", (req, res) => {
+    db.Post.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect("/pets");
+        })
 });
 
 module.exports = router;
