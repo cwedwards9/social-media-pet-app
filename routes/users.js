f86ee9e1a31d674d129f7279aec0001d718cf291
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-12 19:20:40 -0500

    Add register GET route for getting register page and POST route for registering/creating a new user.

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
+const User = require("../models/User");
 
+// Root route - redirect to login screen
 router.get("/", (req, res) => {
-    res.render("landing");
+    res.redirect("users/login");
 });
 
+
+router.get("/register", (req, res) => {
+    res.render("users/register");
+});
+
+router.post("/register", async (req, res) => {
+    try {
+        const { password, username, email, ownerName, petName } = req.body;
+        const user = new User({username, email, ownerName, petName});
+        const registeredUser = await User.register(user, password);
+        res.redirect("/pets");
+    } catch(err) {
+        console.log(err.message);
+        res.redirect("/register");
+    }
+});
 
 
 module.exports = router;
