e4f47bb9247764772823c970bd2939eb392dc885
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-13 10:37:44 -0500

    Add GET and POST routes for /login. Get the login page and post log in credentials and check against database.

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const User = require("../models/User");
 
 // Root route - redirect to login screen
 router.get("/", (req, res) => {
-    res.redirect("users/login");
+    res.redirect("/login");
 });
 
-
+// GET route for register page
 router.get("/register", (req, res) => {
     res.render("users/register");
 });
 
+// POST route for registering/creating a new user
 router.post("/register", async (req, res) => {
     try {
         const { password, username, email, ownerName, petName } = req.body;
@@ -23,6 +25,18 @@ router.post("/register", async (req, res) => {
         res.redirect("/register");
     }
 });
+
+
+// GET route for login page
+router.get("/login", (req, res) => {
+    res.render("users/login");
+});
+
+// POST route for checking log in credentials
+router.post("/login", passport.authenticate("local"), (req, res) => { //{failureFlash: true, failureRedirect: "/login"}
+   res.redirect("/pets");
+});
+
 
 
 module.exports = router;
