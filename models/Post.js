d4390c4081f5a8e0be23c7125b636996c571fc12
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-14 11:03:05 -0500

    Add a user object field that includes an id which references the user model and a username.

--- models/Post.js
+++ models/Post.js
@@ -13,14 +13,14 @@ const PostSchema = new mongoose.Schema ({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    user: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
     }
-    // author: {
-    //     id: {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: "User"
-    //     },
-    //     username: String
-    // }
 });
 
 module.exports = mongoose.model("Post", PostSchema);
