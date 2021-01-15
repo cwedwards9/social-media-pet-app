59b9fa69a5e9c8e7bd0c0c257d076a18702ea67f
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-15 10:24:37 -0500

    Add autorization middleware to check if a user is logged in and if the user is the rightful owner of the post. If not, the user does not have access to the routes that use the middleware.

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const db = require("./models");
+
 // Object of middleware
 const middlewareAuth = {
     // Middleware to restrict routes if a user is not logged in
@@ -9,6 +11,29 @@ const middlewareAuth = {
         }
         // If user is logged in, continue with the request
         next();
+    },
+
+    // Restrict routes if a user is not the authorized user
+    isPostOwner(req, res, next) {
+        // If the user is logged in, continue to check the ownership of the post
+        if(req.isAuthenticated()){
+            db.Post.findById(req.params.id, function(err, foundPost){
+                if(err){
+                    res.redirect("/pets");
+                } else {
+                    // If the desired post's 'author' is equal to the current user, continue the request
+                    if(foundPost.user.id.equals(req.user._id)) {
+                        next();
+                    } else {
+                        // If the user is not the owner of the post, redirect them
+                        res.redirect("/pets")
+                    }
+                }
+            })
+        } else {
+            // If the user isn't authenticated / logged in, redirect them
+            res.redirect("/pets");
+        }
     }
 }
 
